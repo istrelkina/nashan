0cdd48ce1bce361945b35dc539ac13ff15c24c9b
Irina <strelaweb@gmail.com>
2020-05-18 22:00:00 +1000

    added header and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(function(){
 
-
+	$('.slider').slick({
+	  autoplay: true,
+	  arrows:true,	 
+	  dots: false,
+	  fade: true,
+	  autoplaySpeed: 4000, 
+	  slidesToShow: 1,
+	  slidesToScroll: 1,	  
+	});
 });
