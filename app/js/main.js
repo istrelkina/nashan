dc21c363ee41c6cd4fc780ab02a731db3c2548b1
Irina <strelaweb@gmail.com>
2020-05-19 19:42:54 +1000

    added site

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,20 @@ $(function(){
 	  fade: true,
 	  autoplaySpeed: 4000, 
 	  slidesToShow: 1,
-	  slidesToScroll: 1,	  
+	  slidesToScroll: 1,	  	  
+	});
+
+	$('.info__menu-link').on('click', function() {
+
+	    let href = $(this).attr('href');
+
+	    $('html, body').animate({
+	        scrollTop: $(href).offset().top
+	    }, {
+	        duration: 470,   // по умолчанию «400»
+	        easing: "linear" // по умолчанию «swing»
+	    });
+	     
+	    return false;
 	});
 });
